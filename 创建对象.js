86fd1f722211b385630ad00efe6ae881afc14f59
aichishaqima <wuhyduo@163.com>
2020-04-05 21:36:03 +0800

    20200405

--- 创建对象.js
+++ 创建对象.js
@@ -26,3 +26,21 @@ function Person(name,age,job){
 
 var person1 = new Person("Nicholas",29,"Software Engineer");
 var person2 = new Person("Greg",27,"Doctor");
+
+//原型模式
+function Person(){
+}
+Person.prototype.name="Nicholas";
+Person.prototype.age=29;
+Person.prototype.job="Software Engineer";
+Person.prototype.sayName=function(){
+    alert(this.name);
+};
+
+var person1=new Person();
+person1.sayName();  //"Nicholas"
+
+var person2=new Person();
+person2.sayName();  //"Nicholas"
+
+alert(person1.sayName==person2.sayName); //true
